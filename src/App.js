1a4a8920046708d06245ec2cdf575fb504213e47
Shegofa <shogofadeveloper12@gmail.com>
2025-01-17 04:10:35 +0430

    Vercel problem solved

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import React from "react";
 import "./index.css";
 import { ThemeProvider } from "./ThemeContext";
-import Header from "./components/header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Header from './components/Header';
 import "animate.css";
 import "swiper/swiper-bundle.css";
 
